13aa0074dc47b3677f5bf5482b783fe5df182d70
AlexKulazhenko <aakulazhenko@gmail.com>
2023-01-31 14:58:03 +0300

    Delete item name

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -12,7 +12,6 @@ function GoodsItem(props) {
         <div className='card' id={id}>
             <div className='card-image'>
                 <img src={full_background} alt={name} />
-                {/* <span className='card-title'>{name}</span> */}
             </div>
             <div className='card-content'>
                 <p>{description}</p>
