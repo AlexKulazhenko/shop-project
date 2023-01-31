75cb0645d9f9f09e34bbf2fa8551f4ca9a24ea83
AlexKulazhenko <aakulazhenko@gmail.com>
2023-01-31 13:48:14 +0300

    Update

---
+++ src/components/GoodsItem.jsx
@@ -0,0 +1,41 @@
+function GoodsItem(props) {
+    const {
+        id,
+        name,
+        description,
+        price,
+        full_background,
+        addToBasket = Function.prototype,
+    } = props;
+
+    return (
+        <div className='card' id={id}>
+            <div className='card-image'>
+                <img src={full_background} alt={name} />
+                <span className='card-title'>{name}</span>
+            </div>
+            <div className='card-content'>
+                <p>{description}</p>
+            </div>
+            <div className='card-action'>
+                <button
+                    className='btn'
+                    onClick={() =>
+                        addToBasket({
+                            id,
+                            name,
+                            price,
+                        })
+                    }
+                >
+                    Купить
+                </button>
+                <span className='right' style={{ fontSize: '1.8rem' }}>
+                    {price} руб.
+                </span>
+            </div>
+        </div>
+    );
+}
+
+export { GoodsItem };
